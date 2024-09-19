3fdab07acd5deafc3560c53c816c2b1a008b3141
MacPro <macpro@MacBook-Pro-de-MacPro.local>
2024-09-19 16:36:23 -0300

    add a delete into tickets list

--- src/app/pages/tickets/components/tickets-list/tickets-list.component.ts
+++ src/app/pages/tickets/components/tickets-list/tickets-list.component.ts
@@ -34,7 +34,29 @@ export class TicketsListComponent implements OnInit {
       },
       error => this.errorMessage = <any>error
     );
+  }
 
+  deleteTicket(id: string): void {
+    if (id == '') {
+      this.onSaveComplete();
+    } else {
+      if (confirm(`Tem certeza que deseja excluir o ticket: ?`)) {
+        this.ticketsService.delete(id)
+          .subscribe(
+            () => this.onSaveComplete(),
+            (error: any) => this.errorMessage = <any>error
+          );
+      }
+    }
+  }
+
+  onSaveComplete() {
+    this.ticketsService.getTickets().subscribe(
+      tickets => {
+        this.tickets = tickets;
+      },
+      error => this.errorMessage = <any>error
+    );
   }
 
 }
